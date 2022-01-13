d321a692efa714e29a3d4d76182ed2c0ef0d4cad
KdotA <kenan@akinci.io>
2022-01-14 00:54:24 +0100

    finished the app and added some more flexibility such as being able to input the data

--- 16-Async/script.js
+++ 16-Async/script.js
@@ -2,7 +2,14 @@
 
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
+const inputCountry = document.querySelector('.inputCountry');
 
+inputCountry.addEventListener('keyup', function (e) {
+  if (e.keyCode === 13) {
+    e.preventDefault();
+    btn.click();
+  }
+});
 ///////////////////////////////////////
 
 /* const getCountryData = function (country) {
@@ -58,10 +65,10 @@ const renderCountry = function (data, className = '') {
   </article>
   `;
   countriesContainer.insertAdjacentHTML('beforeend', html);
-  countriesContainer.style.opacity = 1;
+  //   countriesContainer.style.opacity = 1;
 };
 
-const getCountryAndNeighbour = function (country) {
+/* const getCountryAndNeighbour = function (country) {
   //AJAX call 1
   const request = new XMLHttpRequest();
   request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
@@ -91,6 +98,70 @@ const getCountryAndNeighbour = function (country) {
       });
     });
   });
+}; */
+
+// getCountryAndNeighbour('greece');
+
+// PROMISES
+
+/* const getCountryDataPromise = function (country) {
+  // Fetch the data with Fetch API
+  fetch(`https://restcountries.com/v3.1/name/${country}`) // -> returns a Promise
+    // if Promise ->
+    .then(response => response.json()) // json() methord returns also another Promise
+    // now render the data with renderCountry() function
+    .then(data => renderCountry(data[0]));
 };
 
-getCountryAndNeighbour('greece');
+getCountryDataPromise('germany');
+ */
+
+const renderError = function (msg) {
+  countriesContainer.insertAdjacentText('beforeend', msg);
+  countriesContainer.style.opacity = 1;
+};
+
+// creating a generic function for getting the JSON to prevent code repetition in the main Promise
+const getJSON = function (url, errorMsg = 'Something went wrong') {
+  return fetch(url).then(response => {
+    console.log(response);
+    if (!response.ok) {
+      throw new Error(`${errorMsg} (${response.status})`); // this error will propagate to the .catch block down there if the response.ok is not true. (.status property is a property of response object)
+    }
+    return response.json(); // return must be used bc of arrow function with block. json() methord returns also another Promise
+  });
+};
+
+const getCountryDataPromise = function (country) {
+  // Fetch the data with Fetch API
+  getJSON(`https://restcountries.com/v3.1/name/${country}`, 'Country not found') // now render the data with renderCountry() function
+    .then(data => {
+      renderCountry(data[0]);
+      const neighbour = data[0].borders;
+
+      return neighbour;
+    })
+    .then(neighbour => {
+      if (!neighbour) {
+        throw new Error(`No neighbour found!`); //throw error if .border property returns nothing (=undefined)
+      }
+      // return every element of neighbour array with forEach and fetch the country data for every element again.
+      return neighbour.forEach(element => {
+        getJSON(
+          `https://restcountries.com/v3.1/alpha/${element}`,
+          'Neighbour countries could not be fetched'
+        ).then(data => {
+          return renderCountry(data[0], 'neighbour'); // render every neighbour
+        });
+      });
+    })
+    .catch(err => renderError(err.message)) //error catching
+    .finally(() => {
+      countriesContainer.style.opacity = 1; //run this nevertheless
+    });
+};
+
+btn.addEventListener('click', () => {
+  countriesContainer.innerHTML = ''; //refresh the list
+  getCountryDataPromise(document.querySelector('.inputCountry').value); //get the value from the input box
+});
